da4d7936f3f371b55d24f8060485e326ff637e86
satriowcksnnnnnn <satriobaguswicaksono28@gmail.com>
2024-05-15 00:42:25 +0700

    update-user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Counter from "./features/counter/Counter";
+import User from "./features/user/User";
 
 function App() {
   return (
     <main className="App">
       <Counter />
+      <User />
     </main>
   );
 }
